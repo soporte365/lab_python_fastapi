8dc232d6d8d48bee3800c719d626230a31f9a167
unknown <soporte.tisi365@gmail.comgit config --global user.name soporte365>
2024-09-01 17:29:05 -0500

    Esdición de archivo test_04

---
+++ Sesion_02/test_04.py
@@ -0,0 +1,14 @@
+"""Tipos de datos complementarios"""
+
+"""Tipos de datos Lista: []"""
+
+var1 = [20, "Mauro", "Calle las Almendras", 1994]
+
+print("El valor de mi var1 es: {}".format(var1))
+print(f"El valor de mi var1 es: {var1}")
+
+"""Valores por índice de la lista"""
+
+print("Valor del índice 0 de mi lista: {}".format(var1[0]))
+print(f"Valor del índice 0 de mi lista: {var1[0]}")
+print("Valor del índice 2 de mi lista: {}".format(var1[2]))
